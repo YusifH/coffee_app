68b611e23e0b6626cf6b0588664e3b87049d8adf
YusifH <yusifhajiyev@yandex.com>
2023-05-15 15:25:36 +0400

    Initial commit

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,63 @@
+import 'package:coffee_app/components/coffee_tile.dart';
+import 'package:coffee_app/models/coffe.dart';
+import 'package:coffee_app/models/coffe_shop.dart';
+import 'package:coffee_app/pages/cart_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ShopPage extends StatelessWidget {
+  const ShopPage({Key? key}) : super(key: key);
+
+  void addToCart(Coffee coffee, BuildContext context) {
+    Provider.of<CoffeeShop>(context, listen: false).addItemCart(coffee);
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Sebete elave olundu'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Bagla'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CoffeeShop>(
+        builder: (context, value, child) => SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(25),
+                child: Column(
+                  children: [
+                    const Text(
+                      'How would you like your coffee?',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: value.coffeeShop.length,
+                        itemBuilder: (context, index) {
+                          Coffee eachCoffee = value.coffeeShop[index];
+                          return CoffeeTile(
+                            onPressed: () => addToCart(eachCoffee, context),
+                            coffee: eachCoffee,
+                            icon: const Icon(Icons.add),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ));
+  }
+}
